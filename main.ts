b61adaa3bffef09b6dfc5a77e9c9784a9e706251
oysa88 <oystein.salomonsen@tekniskmuseum.no>
2021-02-23 18:15:48 +0100

    Startfiler for MakeCode-prosjektet

---
+++ main.ts
@@ -0,0 +1,205 @@
+radio.onReceivedNumber(function (receivedNumber) {
+    LinkStatus = true
+    sistSettAktiv = input.runningTime()
+    if (receivedNumber == 21) {
+        IgniterStatusLP = true
+    } else if (receivedNumber == 22) {
+        IgniterStatusLP = false
+    }
+    if (receivedNumber == 31) {
+        ArmStatusLP = true
+    } else if (receivedNumber == 32) {
+        ArmStatusLP = false
+    }
+})
+function Rearm () {
+    strip.clear()
+    strip.show()
+    while (pins.digitalReadPin(DigitalPin.P1) == 1) {
+        basic.showLeds(`
+            . . . . .
+            . # # # .
+            . # . # .
+            . # # # .
+            . . . . .
+            `)
+        pins.digitalWritePin(DigitalPin.P8, 0)
+        basic.showLeds(`
+            . . . . .
+            . # # # .
+            . # # # .
+            . # # # .
+            . . . . .
+            `)
+        pins.digitalWritePin(DigitalPin.P8, 1)
+    }
+    pins.digitalWritePin(DigitalPin.P8, 0)
+    strip.showColor(neopixel.colors(NeoPixelColors.Purple))
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+    basic.pause(100)
+    Initialize()
+}
+function StatusCheck () {
+    SelfStatus = true
+    if (SelfStatus && LinkStatus && IgniterStatusLP && ArmStatusLP) {
+        Klar = true
+        basic.showLeds(`
+            # . . . #
+            . # . # .
+            . . # . .
+            . # . # .
+            # . . . #
+            `)
+    } else {
+        Klar = false
+        basic.showLeds(`
+            # . . . #
+            # # . # #
+            # . # . #
+            # . . . #
+            # . . . #
+            `)
+    }
+}
+function NeoPixels () {
+    if (SelfStatus) {
+        strip.setPixelColor(0, neopixel.colors(NeoPixelColors.Green))
+    } else {
+        strip.setPixelColor(0, neopixel.colors(NeoPixelColors.Red))
+    }
+    if (LinkStatus) {
+        strip.setPixelColor(1, neopixel.colors(NeoPixelColors.Green))
+    } else {
+        strip.setPixelColor(1, neopixel.colors(NeoPixelColors.Red))
+    }
+    if (IgniterStatusLP) {
+        strip.setPixelColor(2, neopixel.colors(NeoPixelColors.Green))
+    } else {
+        strip.setPixelColor(2, neopixel.colors(NeoPixelColors.Red))
+    }
+    if (ArmStatusLP) {
+        strip.setPixelColor(3, neopixel.colors(NeoPixelColors.Green))
+    } else {
+        strip.setPixelColor(3, neopixel.colors(NeoPixelColors.Red))
+    }
+    if (Klar) {
+        strip.setPixelColor(4, neopixel.colors(NeoPixelColors.Green))
+    } else {
+        strip.setPixelColor(4, neopixel.colors(NeoPixelColors.Red))
+    }
+}
+function Launch () {
+    if (Klar) {
+        basic.showLeds(`
+            . . # . .
+            . # # # .
+            # . # . #
+            . . # . .
+            . . # . .
+            `)
+        radio.sendNumber(42)
+        Rearm()
+    }
+}
+function Initialize () {
+    SelfStatus = false
+    LinkStatus = false
+    ArmStatus = false
+    Klar = false
+    strip.showColor(neopixel.colors(NeoPixelColors.Purple))
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . # . .
+        . . . . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . # # # .
+        . # . # .
+        . # # # .
+        . . . . .
+        `)
+    basic.showLeds(`
+        # # # # #
+        # . . . #
+        # . . . #
+        # . . . #
+        # # # # #
+        `)
+    basic.showLeds(`
+        # . . . #
+        # # . # #
+        # . # . #
+        # . . . #
+        # . . . #
+        `)
+    basic.pause(200)
+}
+function BuzzerBlink () {
+    pins.digitalWritePin(DigitalPin.P13, 1)
+    pins.digitalWritePin(DigitalPin.P14, 1)
+    basic.pause(100)
+    pins.digitalWritePin(DigitalPin.P13, 0)
+    pins.digitalWritePin(DigitalPin.P14, 0)
+    basic.pause(100)
+    pins.digitalWritePin(DigitalPin.P13, 1)
+    pins.digitalWritePin(DigitalPin.P14, 1)
+    basic.pause(100)
+    pins.digitalWritePin(DigitalPin.P13, 0)
+    pins.digitalWritePin(DigitalPin.P14, 0)
+    basic.pause(100)
+    pins.digitalWritePin(DigitalPin.P13, 1)
+    pins.digitalWritePin(DigitalPin.P14, 1)
+    basic.pause(100)
+    pins.digitalWritePin(DigitalPin.P13, 0)
+    pins.digitalWritePin(DigitalPin.P14, 0)
+}
+let ArmStatus = false
+let Klar = false
+let SelfStatus = false
+let ArmStatusLP = false
+let IgniterStatusLP = false
+let sistSettAktiv = 0
+let LinkStatus = false
+let strip: neopixel.Strip = null
+strip = neopixel.create(DigitalPin.P0, 5, NeoPixelMode.RGB)
+radio.setGroup(1)
+radio.setTransmitPower(7)
+pins.digitalWritePin(DigitalPin.P15, 1)
+let oppdateringsfrekvens = 200
+Initialize()
+basic.forever(function () {
+    StatusCheck()
+    if (pins.digitalReadPin(DigitalPin.P5) == 1) {
+        strip.showColor(neopixel.colors(NeoPixelColors.Red))
+        basic.pause(100)
+        StatusCheck()
+    }
+    if (pins.digitalReadPin(DigitalPin.P11) == 0) {
+        Launch()
+    }
+    if (Klar) {
+        pins.digitalWritePin(DigitalPin.P13, 1)
+        pins.digitalWritePin(DigitalPin.P14, 1)
+    } else {
+        pins.digitalWritePin(DigitalPin.P13, 0)
+        pins.digitalWritePin(DigitalPin.P14, 0)
+    }
+})
+control.inBackground(function () {
+    while (true) {
+        radio.sendNumber(11)
+        if (input.runningTime() - sistSettAktiv > 3 * oppdateringsfrekvens) {
+            LinkStatus = false
+        }
+    }
+    basic.pause(oppdateringsfrekvens)
+})
